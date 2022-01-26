6fa1e5706fd346370280eb345a6f312d49154ef8
dd8917vk <jameseroni@gmail.com>
2022-01-25 19:22:59 -0600

    first commit

--- basicpost.sh
+++ basicpost.sh
@@ -1,5 +1,5 @@
 curl -X POST \
-     -H 'Authorization: Bearer xoxb-3002099215157-2990465293223-44qkFoP6pkDBMQfO3Us3SuCY' \
+     -H 'Authorization: Bearer token_here' \
      -H 'Content-type: application/json;charset=utf-8' \
     --data '{"channel":"#general","text":"Hello, Slack!"}' \
 https://slack.com/api/chat.postMessage
