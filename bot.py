47b907248db41a7621f6a78c82162e4e08b23f55
dd8917vk <jameseroni@gmail.com>
2022-01-25 21:33:09 -0600

    added a lot.. lol

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ from requests_html import HTMLSession
 
 class Connector:
     def __init__(self):
+        #Creds in creds.json file, don't forget to add to .gitignore
         self.creds = self.get_creds()
         self.token = self.creds["token"].strip()
         self.signing_secret = self.creds["signing_secret"].strip()
@@ -26,48 +27,108 @@ class Connector:
 class Bot(Connector):
     def __init__(self):
         super().__init__()
-        #Inhereted from Connector
+        #Inhereted from Connector, create web client with slack token
         self.SLACK_BOT_TOKEN = self.token
         self.SLACK_CLIENT = WebClient(self.SLACK_BOT_TOKEN) 
 
     def send_message(self, message):
+        #If you get ssl error, go to your python installation directory and run install certs sh (google it)
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
         sc = WebClient(self.SLACK_BOT_TOKEN,ssl=ssl_context)
-    # make the POST request through the python slack client
-    # check if the request was a success
+        # make the POST request through the python slack client
         try:
             self.SLACK_CLIENT.chat_postMessage(
             channel='#general',
             text=message
             )#.get()
+        #if there is an error, append it to the log
         except SlackApiError as e:
             log_path = os.getcwd()+'/bot.log'
             if not os.path.isfile(log_path):
+                #create logging file if it doesn't exist
                 with open(log_path, 'a') as file:
                     pass
-            # with open(os.path.join(log_path, 'bot.log'), 'w') as f:
-            #     f.write('hello')
+            #log error if slack message doesn't go through
             logging.basicConfig(filename=log_path, level=logging.DEBUG, 
                     format='%(asctime)s %(levelname)s %(name)s %(message)s') 
             logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
             logging.error(e.response)
 
     def scrape(self):
-        # page = requests.get("https://docs.velociraptor.app/exchange/")
-        # soup = BeautifulSoup(page.content, 'html.parser')
         session = HTMLSession()
-        r = session.get('https://docs.velociraptor.app/exchange/')
-        r.html.render()
-        x = r.html.find('div.row')
-        print(len(x[1].find('a.title')))
-        print(x)
+        req = session.get('https://docs.velociraptor.app/exchange/')
+        #Have to call html render to force javascript to load so html can be scraped
+        req.html.render()
+        html = req.html.find('div.row')
+        #This is the number of div's each containing one artifact on the web page
+        current_num_artifacts = len(html[1].find('a.title'))
+        #This is the top artifact on the page
+        top_artifact = html[1].find('a.title')[0]
+        #A link to the top (new or latest) artifact on the page for slack message
+        link_to_artifact = f"https://docs.velociraptor.app/{list(top_artifact.links)[0]}"
+        # return (current_num_artifacts, link_to_artifact)
+        return self.check_new_artifact(current_num_artifacts, link_to_artifact)
+
+    def check_new_artifact(self, current_num_artifacts, link_to_artifact):
+        print(f'Current num artifacts from raptor: {current_num_artifacts}')
+        prev_count = os.getcwd()+'/prev_count.txt'
+        try:
+        #try reading previous count artifacts from static text file to compare to new num from exchange
+            with open(prev_count, 'r') as file:
+                old_count = int(file.readlines()[0])
+                print(f'Old count artifacts from text file: {old_count}')
+                if old_count != current_num_artifacts:
+                    print('counts are different, do something')
+                    self.send_message("```Standby for a new artifact...```")
+                    time.sleep(1)
+                    self.pureimg()
+        #if file with num_artifacts doesn't exist, create it and write the num_artifacts
+        except FileNotFoundError:
+            with open(prev_count, 'w') as file:
+                file.writelines(str(current_num_artifacts))
+        if not os.path.isfile(prev_count):
+            with open(prev_count, 'w') as file:
+                file.writelines(str(current_num_artifacts))
+
+    def pureimg(self):
+        try:
+            response = self.SLACK_CLIENT.files_upload(file='clever.png', initial_comment='```Clever Girl...```', channels='general')
+        except SlackApiError as e:
+    # You will get a SlackApiError if "ok" is False
+            assert e.response["ok"] is False
+            assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+            print(f"Got an error: {e.response['error']}")
+
+    # def pureimg(self):
+    #     def subimg(data):
+    #         data = '[{"text": "", "image_url": "'+data+'"}]'
+    #         data = [json.loads(data[1:-1])]
+    #         return data
+
+    #     slacker = WebClient(token=self.token)
+    #     #This function will make the image url to correct format.
+    #     clever = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'clever.png')
+    #     print(clever)
+    #     response = slacker.files_upload(channel='#general',file=clever)
+    #     clever = response['file']['permalink']
+
+    #     #It gives cross OS compatibility on filepath.
+    #     slacker.chat_postMessage(channel='general', text="Sample Text", username='testbot', attachments=subimg(clever), icon_emoji=':lizard:')
+
+
+        # print(current_num_artifacts)
+        # print(html)
 
 if __name__ == "__main__":
     b = Bot()
-    b.send_message("hello")
-    # print(b.scrape())
+    print(b.scrape())
+    # b.send_message("hello")
+    # messages = ["hello", "thomas", "nichols", "I am a bot from james' imagination"]
+    # for m in messages:
+    #     b.send_message(m)
+    #     time.sleep(1)
     # messages = ["hello", "This is 2", "This is from a loop"]
     # for m in messages:
     #     b.send_message(m)
